24acba0f356edb9e578e4d2825053f70fa0312b7
Tufan <tufanpande@gmail.com>
2024-01-21 15:17:27 +0545

    Email and Password Validation

---
+++ 19jan-ex/index.js
@@ -0,0 +1,32 @@
+
+      function checkEmailCriteria() {
+        const email = document.getElementById("Email").value;
+        const emailResult = document.getElementById("emailResult");
+
+        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (pattern.test(email)) {
+          emailResult.textContent = "Email meets criteria";
+          emailResult.style.color = "green";
+        } else {
+          emailResult.textContent = "Email does not meet criteria";
+          emailResult.style.color = "red";
+        }
+      }
+
+      function checkPasswordCriteria() {
+        const password = document.getElementById("Password").value;
+        const passwordResult = document.getElementById("passwordResult");
+
+        const pattern =
+          /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
+
+        if (pattern.test(password)) {
+          passwordResult.textContent = "Password meets criteria";
+          passwordResult.style.color = "green";
+        } else {
+          passwordResult.textContent = "Password does not meet criteria";
+          passwordResult.style.color = "red";
+        }
+      }
+    

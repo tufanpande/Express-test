cfc9edf6158b78954e208519859ffbdad6268f2a
Tufan <tufanpande@gmail.com>
2024-01-22 18:16:14 +0545

    index

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 
+
+const indexRouter = require("./routes");
 const app = express();
 
-// printing Hello world 
-app.get("/",(req, res)=>{
-    res.json({msg:"Hello world"});
-});
-app.get("/tufan",(req, res)=>{
-    res.json({msg:"Hello tufan"});
-});
-
+app.use("/", indexRouter);
 app.listen(8000,()=>{
     console.log("Application is running");
 });
+
+
+
+
+
+// GET => server to fE
+//POST => FE to server
+//delete=> FE to server delete my 1 data
+// PUT => FE to server ; fix majority property of data
+// PATCH => FE to server ma single data change garney hai
+
+
+
+

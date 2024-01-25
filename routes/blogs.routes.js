252867ff7a646070505eed13d1ad09910a1dae75
Tufan <tufanpande@gmail.com>
2024-01-25 19:55:22 +0545

    ROUTES

--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -3,4 +3,10 @@ const router= require("express").Router();
 router.get("/", (req, res)=>{
     res.json({msg:"hello from blogs"});
 });
+
+router.post("/",(req,res)=>{
+
+    console.log(req.query);
+    console.log({query:req.query});
+});
 module.exports=router;

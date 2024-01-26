114045fc7aeed0b5c7ac4c0844d5048a096f2a8e
Tufan <tufanpande@gmail.com>
2024-01-26 17:38:56 +0545

    routes

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,10 +1,10 @@
 const router= require("express").Router();
-const {numberFormatter}= require("../utils/numberFormatter");
+// const {numberFormatter}= require("../utils/numberFormatter");
 
 router.get("/", (req, res)=>{
     res.json({msg:"hello from book"});
 });
-router.get("/:id", (req, res,next)=>{
+router.get("/", (req, res,next)=>{
 
     try{
         const {id }=req.params;
@@ -15,6 +15,33 @@ router.get("/:id", (req, res,next)=>{
         next(e);
     }
 });
+
+router.post("/", (req, res,next)=>{
+
+    try{
+        const data =Object.keys(req.body).length;
+
+        if(!data) throw new Error("Body is missing");
+        res.json({msg:`Hello from book id ${data}`});
+
+
+    }catch(e){
+        next(e);
+    }
+});
+router.put("/:id", (req, res,next)=>{
+
+    try{
+        const {id }=req.params;
+        const {name} = req.body;
+        if(!id || !name) throw new Error("Id or name is missing");
+        res.json({msg:`Hello from book id ${id} of book name ${name}`});
+    }catch(e){
+        next(e);
+    }
+});
+
+
 // router.get("/:name",(req,res,next)=>{
 //     try{
 //         const{name}=req.params;
@@ -47,4 +74,9 @@ router.get("/:id", (req, res,next)=>{
 //         );
 //     res.json({msg:result});
 // });
+router.post("/:random", (req, res) => {
+    //make a random number console using callback function in express js
+    const ran = Math.floor(Math.random() * 17);
+    res.json({ msg: `${ran}` });
+  });
 module.exports=router;

e1de12a84ec3b03538c268f6222d0bb01c743830
Tufan <tufanpande@gmail.com>
2024-01-28 16:55:34 +0545

    routes

--- routes/book.route.js
+++ routes/book.route.js
@@ -34,7 +34,7 @@ router.put("/:id", (req, res,next)=>{
     try{
         const {id }=req.params;
        
-        if(!id || !name) throw new Error("Id or name is missing");
+        // if(!id || !name) throw new Error("Id or name is missing");
         res.json({msg:"Update multiple props of books"});
     }catch(e){
         next(e);
@@ -45,7 +45,7 @@ router.patch("/:id", (req, res,next)=>{
     try{
         const {id }=req.params;
        
-        if(!id) throw new Error("Id  is missing");
+        // if(!id) throw new Error("Id  is missing");
         res.json({msg:"update single item of books"});
     }catch(e){
         next(e);

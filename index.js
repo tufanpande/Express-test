10015368fdfb9d1ea9aabc22ddf49028c4aeb7a0
Tufan <tufanpande@gmail.com>
2024-01-30 17:33:22 +0545

    Main

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const indexRouter = require("./routes");
 
 const app = express();
+app.use(express.static("public"));
+
 app.use(express.json());
 
 // app.use((res, req, next)=>{

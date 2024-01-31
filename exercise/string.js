f11e08ccaf81fc9f09215f4484d41384985d629b
Tufan <tufanpande@gmail.com>
2024-01-31 16:25:49 +0545

    exercise

--- exercise/string.js
+++ exercise/string.js
@@ -1,6 +1,6 @@
 
 //Write a JavaScript function to get the number of occurrences 
-//22of each letter in a specified string.
+//of each letter in a specified string.
 const CLetters = (inputString) =>
   inputString
     .replace(/[^a-zA-Z]/g, '')

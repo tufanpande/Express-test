960f10a8e9c3db0c93d2169708f16c7f629aeba5
Tufan <tufanpande@gmail.com>
2024-02-02 18:57:36 +0545

    Blogs

--- modules/blogs/blog.model.js
+++ modules/blogs/blog.model.js
@@ -1,14 +1,20 @@
 
 
 // schema=>model => controller=>api()
-const mongoose =require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const BlogSchema = new mongoose.Schema({
-    title: {type: string},
-    author: {},
-    publishedDate:{},
-    categories:{},
-    wordCount: {},
-    time:{},
-    featuredImages:{},
+const BlogSchema = new Schema({
+  id: { type: Number, require: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publishedDate: { type: Date, default: Date.now() },
+  categories: { type: Array },
+  words: { type: Number, required: true },
+  time: { type: Number, required: true },
+  content: { type: String, required: true },
+  featuredImage: { type: String },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() },
 });
+
+module.exports = model("Blog", BlogSchema);
--- modules/blogs/blog.route.js
+++ modules/blogs/blog.route.js
@@ -1,23 +1,25 @@
 
+const router = require("express").Router();
+const blogController = require("./blog.controller");
 
-const router= require("express").Router();
-router.get("/", (req, res, next) => {
-    
-    res.json({ msg: "Welcome to blog route" });
-    next();
-  });
-  router.post("/",(req,res,next)=>{
-    try{
-        const data = req.body;
-        data.time = Math.floor(data.words/ 238);
-        console.log({data});
-        res.json({msg: "Hello form blog post router"});
-
-
-    }catch(e){
-        next(e);
-    }
+router.get("/blog", (req, res, next) => {
+  try {
+    res.json({ msg: "Hello from Blog Route" });
+  } catch (e) {
+    next(e);
+  }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const data = req.body;
+    data.time = Math.floor(data.words / 238);
+    console.log({ data });
+    const result = await blogController.create(data);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
 
-module.exports=router;
+module.exports = router;
